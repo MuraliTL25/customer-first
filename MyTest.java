33c4e56c9455b6d4fdbc03f7d3145bccda5eca0a
MuraliTL25 <39365053+MuraliTL25@users.noreply.github.com>
2018-07-21 21:52:10 +0530

    Create MyTest.java

---
+++ MyTest.java
@@ -0,0 +1,10 @@
+public class MyTest{
+  public static void main(String ag[]){
+    System.out.println("MyTest main method started------->");
+      int favnum= 26;
+          System.out.println("fav number------->"+favnum);
+
+        System.out.println("MyTest main method end------->");
+
+  }
+}
